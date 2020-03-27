bca4365ddc8e2c07a93f1d89d810a56f9b5d2066
rllola <rllola80@gmail.com>
2020-03-27 11:27:04 +0100

    rustfmt

--- signer-java/src/lib.rs
+++ signer-java/src/lib.rs
@@ -1,7 +1,7 @@
 use filecoin_signer;
-use jni::JNIEnv;
 use jni::objects::{JClass, JString, JValue};
-use jni::sys::{jstring, jobject};
+use jni::sys::{jobject, jstring};
+use jni::JNIEnv;
 //use jni::Result;
 
 #[repr(C)]
@@ -18,27 +18,37 @@ impl ExtendedKey {
 pub extern "system" fn Java_Signer_generate_1mnemonic(env: JNIEnv, _class: JClass) -> jstring {
     let mnemonic = filecoin_signer::key_generate_mnemonic().unwrap();
 
-    let output = env.new_string(mnemonic.0)
+    let output = env
+        .new_string(mnemonic.0)
         .expect("Couldn't create java string!");
 
     output.into_inner()
 }
 
 #[no_mangle]
-pub extern "system" fn Java_Signer_key_1derive(env: JNIEnv, _class: JClass, mnemonic: JString, path: JString) -> jobject {
+pub extern "system" fn Java_Signer_key_1derive(
+    env: JNIEnv,
+    _class: JClass,
+    mnemonic: JString,
+    path: JString,
+) -> jobject {
+    let mnemonic_rust: String = env
+        .get_string(mnemonic)
+        .expect("Couldn't get the mnemonic java string!")
+        .into();
 
-    let mnemonic_rust: String =
-     env.get_string(mnemonic).expect("Couldn't get the mnemonic java string!").into();
+    let path_rust: String = env
+        .get_string(path)
+        .expect("Couldn't get the path java string!")
+        .into();
 
-    let path_rust: String =
-     env.get_string(path).expect("Couldn't get the path java string!").into();
+    let ext_key =
+        filecoin_signer::key_derive(filecoin_signer::Mnemonic(mnemonic_rust), path_rust).unwrap();
 
-     let ext_key = filecoin_signer::key_derive(filecoin_signer::Mnemonic(mnemonic_rust), path_rust).unwrap();
+    let extended_key = ExtendedKey { 0: ext_key };
 
-     let extended_key = ExtendedKey { 0: ext_key };
+    // This call the java constructor of the ExtendedKey java class in Signer.java (normaly)
+    let output = env.new_object("ExtendedKey", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)LExtendedKey", &extended_key).unwrap();
 
-     // This call the java constructor of the ExtendedKey java class in Signer.java (normaly)
-     let output = env.new_object("ExtendedKey", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)LExtendedKey", &extended_key).unwrap();
-
-     output.into_inner()
+    output.into_inner()
 }
